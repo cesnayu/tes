b5e394739eff1af153d37a49833fe8e2245fb1ec
cesnayu <53113207+cesnayu@users.noreply.github.com>
2026-04-15 18:08:03 +0700

    Update I.py

--- I.py
+++ I.py
@@ -17,13 +17,14 @@ def load_data():
 def save_data(data): 
    with open(DATA_FILE, "w") as f: json.dump(data, f)
 
-def get_stock_data(ticker): 
-    stock = yf.Ticker(ticker) 
+def get_stock_data(ticker):
+    stock = yf.Ticker(ticker)
     info = stock.history(period="2d")
-if len(info) < 2: 
-    return None
-
-return info
+    
+    if len(info) < 2:
+        return None
+        
+    return info
 
 current_price = info["Close"].iloc[-1]
 prev_close = info["Close"].iloc[-2]
